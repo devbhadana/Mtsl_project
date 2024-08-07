9e7479ed20010b1cd2a91d4c10749d185de2d4d9
devbhadana <93996342+devbhadana@users.noreply.github.com>
2024-08-07 22:13:35 +0530

    remove redundancy

--- script.js
+++ script.js
@@ -1,102 +1,111 @@
-var selectedRow = null
-
+var selectedRow = null;
+let arr = [];
 function onFormSubmit(e) {
-	event.preventDefault();
-	const isValid = validateForm();
-	if(!isValid){
-		return;
-	}
-	var formData = readFormData();
-        if (selectedRow == null){
-            insertNewRecord(formData);
-		}
-        else{
-            updateRecord(formData);
-		}
-        resetForm();    
-}
-function validateForm(){
-	var salary = document.getElementById('salary').value;
-	var salaryRegex = /^\d+$/;
-	var contact = document.getElementById('contact').value;
-	var contactRegex =  /^(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\/]?){0,})(?:[\-\.\ \\\/]?(?:#|ext\.?|extension|x)[\-\.\ \\\/]?(\d+))?$/i;
-	
+  event.preventDefault();
+  const isValid = validateForm();
+  if (!isValid) {
+    return;
+  }
+  var formData = readFormData();
 
-	if (!salaryRegex.test(salary) || salary <= 0) {
-                alert('Please enter a valid salary');
-		
-                return false;
-	}
-	if(!contactRegex.test(contact) ){
-		alert('enter a valid mobile number');
-		return false;
-	}
-	else{
-	return true;
-	}
+  const id = formData.empCode;
+  if (arr.includes(id)) {
+    alert("Already exist");
+    return;
+  } else {
+    arr.push(id);
+  }
+
+  if (selectedRow == null) {
+    insertNewRecord(formData);
+  } else {
+    updateRecord(formData);
+  }
+  resetForm();
+}
+function validateForm() {
+  var salary = document.getElementById("salary").value;
+  var salaryRegex = /^\d+$/;
+  var contact = document.getElementById("contact").value;
+  var contactRegex =
+    /^(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\/]?){0,})(?:[\-\.\ \\\/]?(?:#|ext\.?|extension|x)[\-\.\ \\\/]?(\d+))?$/i;
+
+  if (!salaryRegex.test(salary) || salary <= 0) {
+    alert("Please enter a valid salary");
+
+    return false;
+  }
+  if (!contactRegex.test(contact)) {
+    alert("enter a valid mobile number");
+    return false;
+  } else {
+    return true;
+  }
 }
 
 //Retrieve the data
 function readFormData() {
-    var formData = {};
-    formData["empCode"] = document.getElementById("empCode").value;
-    formData["name"] = document.getElementById("name").value;
-    formData["email"] = document.getElementById("email").value;
-    formData["contact"] = document.getElementById("contact").value;
-    formData["salary"] = document.getElementById("salary").value;
-    return formData;
+  var formData = {};
+  formData["empCode"] = document.getElementById("empCode").value;
+  formData["name"] = document.getElementById("name").value;
+  formData["email"] = document.getElementById("email").value;
+  formData["contact"] = document.getElementById("contact").value;
+  formData["salary"] = document.getElementById("salary").value;
+  return formData;
 }
 
 //Insert the data
 function insertNewRecord(data) {
-    var table = document.getElementById("storeList").getElementsByTagName('tbody')[0];
-    var newRow = table.insertRow(table.length);
-    cell1 = newRow.insertCell(0);
-		cell1.innerHTML = data.empCode;
-    cell2 = newRow.insertCell(1);
-		cell2.innerHTML = data.name;
-    cell3 = newRow.insertCell(2);
-		cell3.innerHTML = data.email;
-    cell4 = newRow.insertCell(3);
-		cell4.innerHTML = data.contact;
-    cell5 = newRow.insertCell(4);
-		cell5.innerHTML = data.salary;
-    cell5 = newRow.insertCell(5);
-        cell5.innerHTML = `<button onClick="onEdit(this)">Edit</button> <button onClick="onDelete(this)">Delete</button>`;
+  var table = document
+    .getElementById("storeList")
+    .getElementsByTagName("tbody")[0];
+  var newRow = table.insertRow(table.length);
+  cell1 = newRow.insertCell(0);
+  cell1.innerHTML = data.empCode;
+  cell2 = newRow.insertCell(1);
+  cell2.innerHTML = data.name;
+  cell3 = newRow.insertCell(2);
+  cell3.innerHTML = data.email;
+  cell4 = newRow.insertCell(3);
+  cell4.innerHTML = data.contact;
+  cell5 = newRow.insertCell(4);
+  cell5.innerHTML = data.salary;
+  cell5 = newRow.insertCell(5);
+  cell5.innerHTML = <button onClick="onEdit(this)">Edit</button> <button onClick="onDelete(this)">Delete</button>;
 }
 
 //Edit the data
 function onEdit(td) {
-    selectedRow = td.parentElement.parentElement;
-    document.getElementById("empCode").value = selectedRow.cells[0].innerHTML;
-    document.getElementById("name").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("email").value = selectedRow.cells[2].innerHTML;
-    document.getElementById("contact").value = selectedRow.cells[3].innerHTML;
-    document.getElementById("salary").value = selectedRow.cells[4].innerHTML;
+  selectedRow = td.parentElement.parentElement;
+  document.getElementById("empCode").value = selectedRow.cells[0].innerHTML;
+  document.getElementById("name").value = selectedRow.cells[1].innerHTML;
+  document.getElementById("email").value = selectedRow.cells[2].innerHTML;
+  document.getElementById("contact").value = selectedRow.cells[3].innerHTML;
+  document.getElementById("salary").value = selectedRow.cells[4].innerHTML;
 }
 function updateRecord(formData) {
-    selectedRow.cells[0].innerHTML = formData.empCode;
-    selectedRow.cells[1].innerHTML = formData.name;
-    selectedRow.cells[2].innerHTML = formData.email;
-    selectedRow.cells[3].innerHTML = formData.contact;
-    selectedRow.cells[4].innerHTML = formData.salary;
+  selectedRow.cells[0].innerHTML = formData.empCode;
+  selectedRow.cells[1].innerHTML = formData.name;
+  selectedRow.cells[2].innerHTML = formData.email;
+  selectedRow.cells[3].innerHTML = formData.contact;
+  selectedRow.cells[4].innerHTML = formData.salary;
 }
 
 //Delete the data
 function onDelete(td) {
-    if (confirm('Do you want to delete this record?')) {
-        row = td.parentElement.parentElement;
-        document.getElementById('storeList').deleteRow(row.rowIndex);
-        resetForm();
-    }
+  if (confirm("Do you want to delete this record?")) {
+    row = td.parentElement.parentElement;
+    document.getElementById("storeList").deleteRow(row.rowIndex);
+    resetForm();
+  }
 }
 
 //Reset the data
 function resetForm() {
-    document.getElementById("empCode").value = '';
-    document.getElementById("name").value = '';
-    document.getElementById("email").value = '';
-    document.getElementById("contact").value = '';
-    document.getElementById("salary").value = '';
-    selectedRow = null;
+  document.getElementById("empCode").value = "";
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("contact").value = "";
+  document.getElementById("salary").value = "";
+  selectedRow = null;
 }
